086a76527600c273d91f288f34270e37e75876dc
florensh <florensh@gmx.de>
2016-03-20 13:15:53 +0100

    added query for signals

--- src/main/java/com/fh/locating/CorsFilter.java
+++ src/main/java/com/fh/locating/CorsFilter.java
@@ -20,8 +20,7 @@ class CorsFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(HttpServletRequest request,
 			HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		response.addHeader("Access-Control-Allow-Origin",
-				"http://localhost:3000");
+		response.addHeader("Access-Control-Allow-Origin", "*");
 
 		if (request.getHeader("Access-Control-Request-Method") != null
 				&& "OPTIONS".equals(request.getMethod())) {
