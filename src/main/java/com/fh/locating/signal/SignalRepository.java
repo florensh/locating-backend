e6f046ed34cec2e02a1469950edefa5c875ae90e
florensh <florensh@gmx.de>
2016-03-20 11:16:07 +0100

    added query for signals

--- src/main/java/com/fh/locating/signal/SignalRepository.java
+++ src/main/java/com/fh/locating/signal/SignalRepository.java
@@ -5,10 +5,11 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface SignalRepository extends MongoRepository<Signal, String> {
 
-	List<Signal> findByTimestampBetweenAndMacIn(Date start, Date end,
-			Collection<String> macs);
+	List<Signal> findByTimestampBetweenAndMacIn(@Param("start") Date start,
+			@Param("end") Date end, @Param("macs") Collection<String> macs);
 
 }
