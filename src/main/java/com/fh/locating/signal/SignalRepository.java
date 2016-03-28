0f86ddf12ad8c98493da063cf34005f75a88b980
florensh <florensh@gmx.de>
2016-03-28 12:28:30 +0200

    changed iso date to dateTime

--- src/main/java/com/fh/locating/signal/SignalRepository.java
+++ src/main/java/com/fh/locating/signal/SignalRepository.java
@@ -6,15 +6,11 @@ import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 public interface SignalRepository extends MongoRepository<Signal, String> {
 
-	List<Signal> findByTimestampBetweenAndMacIn(
-			@DateTimeFormat(iso = ISO.DATE_TIME) @Param("start") Date start,
-			@DateTimeFormat(iso = ISO.DATE_TIME) @Param("end") Date end,
-			@Param("macs") Collection<String> macs);
+	List<Signal> findByTimestampBetweenAndMacIn(@Param("start") Date start,
+			@Param("end") Date end, @Param("macs") Collection<String> macs);
 
 	List<Signal> findByMacIn(@Param("macs") Collection<String> macs);
 
