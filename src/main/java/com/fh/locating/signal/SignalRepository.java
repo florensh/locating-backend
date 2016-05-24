24b96d4360f3860dccb6aac45cc1153a25ff97be
florensh <florensh@gmx.de>
2016-05-24 20:31:27 +0200

    added pushover notification for signals

--- src/main/java/com/fh/locating/signal/SignalRepository.java
+++ src/main/java/com/fh/locating/signal/SignalRepository.java
@@ -6,15 +6,11 @@ import java.util.List;
 import org.joda.time.DateTime;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 public interface SignalRepository extends MongoRepository<Signal, String> {
 
-	List<Signal> findByTimestampBetweenAndMacIn(
-			@DateTimeFormat(iso = ISO.DATE) @Param("start") DateTime start,
-			@DateTimeFormat(iso = ISO.DATE) @Param("end") DateTime end,
-			@Param("macs") Collection<String> macs);
+	List<Signal> findByTimestampBetweenAndMacIn(@Param("start") DateTime start,
+			@Param("end") DateTime end, @Param("macs") Collection<String> macs);
 
 	List<Signal> findByMacIn(@Param("macs") Collection<String> macs);
 
