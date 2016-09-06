5b228db02e321a6529106cbcee8aca5e60f25f1a
florensh <florensh@gmx.de>
2016-09-06 20:21:50 +0200

    debugging heroku build error

--- src/test/java/com/fh/locating/people/PeopleApiTest.java
+++ src/test/java/com/fh/locating/people/PeopleApiTest.java
@@ -1,17 +1,25 @@
 package com.fh.locating.people;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentation;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -21,7 +29,6 @@ import com.fh.locating.Application;
 @SpringApplicationConfiguration(classes = { Application.class })
 @WebAppConfiguration
 @ActiveProfiles("test")
-@IntegrationTest
 public class PeopleApiTest {
 
 	@Autowired
@@ -38,29 +45,29 @@ public class PeopleApiTest {
 
 	@Before
 	public void setUp() {
-		// this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
-		// .apply(documentationConfiguration(this.restDocumentation))
-		// .build();
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+				.apply(documentationConfiguration(this.restDocumentation))
+				.build();
 	}
 
 	@Test
 	public void addPerson() throws Exception {
-		// Person p = new Person();
-		// p.setName("Klaus");
-		//
-		// Device iPhone = new Device();
-		// iPhone.setName("iPhone");
-		// iPhone.setMac("d0:e1:40:ff:ff:ff");
-		//
-		// p.setDevices(Arrays.asList(iPhone));
-		//
-		// this.mockMvc
-		// .perform(
-		// post("/people/")
-		// .contentType(MediaType.APPLICATION_JSON)
-		// .content(
-		// this.objectMapper.writeValueAsString(p)))
-		// .andExpect(status().isCreated()).andDo(document("post-person"));
+		Person p = new Person();
+		p.setName("Klaus");
+
+		Device iPhone = new Device();
+		iPhone.setName("iPhone");
+		iPhone.setMac("d0:e1:40:ff:ff:ff");
+
+		p.setDevices(Arrays.asList(iPhone));
+
+		this.mockMvc
+				.perform(
+						post("/people/")
+								.contentType(MediaType.APPLICATION_JSON)
+								.content(
+										this.objectMapper.writeValueAsString(p)))
+				.andExpect(status().isCreated()).andDo(document("post-person"));
 
 	}
 }
